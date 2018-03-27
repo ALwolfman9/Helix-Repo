cc42cccff09496a964edca7f6f83b161018f281c
Eli Cunningham <etcunningham98@gmail.com>
2018-03-26 20:00:00 -0400

    Added CommandLine and CommandLineAdmin classes

--- Helix_Hospital/src/DB/DBController.java
+++ Helix_Hospital/src/DB/DBController.java
@@ -12,18 +12,27 @@ public class DBController {
 		try {
 			Statement st = conn.createStatement();
 			st.execute("CREATE TABLE EMPLOYEE (" +
-								"firstName VARCHAR(20) NOT NULL," +
-								"middleInitial CHAR(1)," +
-								"lastName VARCHAR(30)," +
-								"phone INTEGER(10)," +
-								"sex ENUM('M', 'F', 'O')," +
-								"ssn INTEGER(10) NOT NULL UNIQUE," +
-								"email VARCHAR(50)," +
-								"address VARCHAR(255)," +
-								"type VARCHAR(20) NOT NULL," +
-								"username VARCHAR(30) PRIMARY KEY" +
+							"	firstName VARCHAR(20) NOT NULL," +
+							"	middleInitial CHAR(1)," +
+							"	lastName VARCHAR(30) NOT NULL," +
+							"	phone INTEGER(10)," +
+							"	sex ENUM('M', 'F', 'O')," +
+							"	ssn INTEGER(10) NOT NULL UNIQUE," +
+							"	email VARCHAR(50)," +
+							"	address VARCHAR(255)," +
+							"	type VARCHAR(20) NOT NULL," +
+							"	username VARCHAR(30) PRIMARY KEY FJD" +
 							");");
-			
+			st.execute("CREATE TABLE DOCTOR (" +
+							"	username VARCHAR(30) PRIMARY KEY FOREIGN KEY REFERENCES EMPLOYEE," +
+							"	specialization VARCHAR(30)" +
+							");");
+			st.execute("CREATE TABLE PATIENT (" +
+					"	patientID INTEGER(10) PRIMARY KEY," +
+					"	roomNumber INTEGER(5)," +
+					"	firstName VARCHAR(20 NOT NULL," +
+					"	lastName VARCHAR(30)");
+
 		} catch (SQLException e) {
 			return false;
 		}
---
+++ Helix_Hospital/src/Main/CommandLine.java
@@ -0,0 +1,82 @@
+package Main;
+
+import Model.*;
+
+import java.util.Scanner;
+
+public class CommandLine {
+
+    Hospital hospital;
+
+    public CommandLine(Hospital hospital){
+        this.hospital = hospital;
+    }
+
+    public void run(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the Hospital!");
+        while(true){
+            printHelp();
+            String cmd = in.next();
+
+            switch(cmd){
+                case "l":
+                case "L":
+                case "login":
+                    login();
+                    break;
+                case "q":
+                case "Q":
+                case "quit":
+                    return;
+                default:
+                    System.out.println("That command was not recognized");
+            }
+        }
+    }
+
+    private void printHelp(){
+        System.out.println("Usage: login/l/L | quit/q/Q");
+        System.out.println("login/l/L\t\t\tLog in to the Hospital Application");
+        System.out.println("quit/q/Q\t\t\tQuit the Hospital Application");
+    }
+
+    /**
+     * Logs a user into the system.
+     */
+    private void login(){
+        Scanner in = new Scanner(System.in);
+
+        CommandLine cmdLine;
+
+        System.out.println("Please log in: ");
+        String username = in.next();
+        if(username.equals("admin")) cmdLine = new CommandLineAdmin(hospital);
+        Employee emp = hospital.getEmployee(username);
+
+        while(emp == null){
+            System.out.println("Username was incorrect. Please try again: ");
+            username = in.next();
+            if(username.equals("admin")) cmdLine = new CommandLineAdmin(hospital);
+            emp = hospital.getEmployee(username);
+        }
+        switch(emp.type){
+            case DOCTOR:
+                cmdLine = new CommandLineDoctor(hospital, emp);
+                break;
+            case NURSE:
+                cmdLine = new CommandLineNurse(hospital, emp);
+                break;
+            case SUPPORT:
+                cmdLine = new CommandLineSupport(hospital, emp);
+                break;
+        }
+        cmdLine.run();
+    }
+
+    private void runAdmin(){
+        System.out.println("Welcome Admin!");
+        System.out.println("");
+    }
+
+}
